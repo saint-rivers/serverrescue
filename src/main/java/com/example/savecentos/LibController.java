e166c77bb97127d782aafcdf6bdc11bb4df88621
Dayan Eam <eam.dayan@gmail.com>
2022-03-16 17:09:27 +0700

    update

--- src/main/java/com/example/savecentos/LibController.java
+++ src/main/java/com/example/savecentos/LibController.java
@@ -7,21 +7,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
+import java.util.Arrays;
 
 @RestController
 public class LibController {
 
     @GetMapping(value = "/test", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity<?> getLib() {
+    public byte[] getLib() {
         String tmp = "/lib64/liblber-2.4.so.2";
         String filename = "/home/saintrivers/howToStartNetwork.txt";
         InputStream in = getClass().getResourceAsStream(tmp);
         try {
-            assert in != null;
-            return ResponseEntity.ok(in.readAllBytes());
+//            assert in != null;
+//            System.out.println(Arrays.toString(in.readAllBytes()));
+//            return in.readAllBytes();
+
+            File path = new File(tmp);
+            return method(path);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return ResponseEntity.notFound().build();
+        return "not working".getBytes();
+    }
+
+    public static byte[] method(File file) throws IOException {
+
+        // Creating an object of FileInputStream to
+        // read from a file
+        FileInputStream fl = new FileInputStream(file);
+
+        // Now creating byte array of same length as file
+        byte[] arr = new byte[(int) file.length()];
+
+        // Reading file content to byte array
+        // using standard read() method
+        fl.read(arr);
+
+        // lastly closing an instance of file input stream
+        // to avoid memory leakage
+        fl.close();
+
+        // Returning above byte array
+        return arr;
     }
 }
