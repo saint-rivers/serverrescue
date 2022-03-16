6f8906cf51110e8533d78ef156f5c2b4464a3a8a
Dayan Eam <eam.dayan@gmail.com>
2022-03-16 16:25:41 +0700

    first commit

---
+++ src/main/java/com/example/savecentos/LibController.java
@@ -0,0 +1,27 @@
+package com.example.savecentos;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+
+@RestController
+public class LibController {
+
+    @GetMapping(value = "/test", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<?> getLib() {
+        String tmp = "/lib64/liblber-2.4.so.2";
+        String filename = "";
+        InputStream in = getClass().getResourceAsStream("/home/saintrivers/howToStartNetwork.txt");
+        try {
+            assert in != null;
+            return ResponseEntity.ok(in.readAllBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
---
+++ src/main/java/com/example/savecentos/SaveCentosApplication.java
@@ -0,0 +1,13 @@
+package com.example.savecentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveCentosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaveCentosApplication.class, args);
+    }
+
+}
